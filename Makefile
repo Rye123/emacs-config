2947a25a561a169fea1bf8f20fd987e17017cf4b
Ryan <ryantoh99@gmail.com>
2024-02-02 10:11:09 +0800

    update config

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 all:
 	mkdir -p ~/.config/emacs
+	cp ./early-init.el ~/.config/emacs/early-init.el
 	cp ./init.el ~/.config/emacs/init.el
 	mkdir -p ~/org
 	mkdir -p ~/.backups
--- init.el
+++ init.el
@@ -1,8 +1,7 @@
-;; Packages
 (require 'package)
-(add-to-list 'package-archives  '("nongnu" . "https://elpa.nongnu.org/nongnu/"))
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(unless package-archive-contents  (package-refresh-contents t))
+(unless package-archive-contents
+  (package-refresh-contents t))
+(package-install-selected-packages 'noconfirm)
 
 ;; Display Customisation
 (load-theme 'manoj-dark t)
@@ -11,74 +10,7 @@
 (global-display-line-numbers-mode t) ;; line numbering
 (electric-pair-mode t) ; pair parentheses
 
-;;; vertico: Completion framework
-(unless (package-installed-p 'vertico)
-  (package-install 'vertico))
-(vertico-mode t) ; completing by narrowing
-
-;;; company: Code Completion Suggestions framework
-(unless (package-installed-p 'company)
-  (package-install 'company))
-(add-hook 'after-init-hook 'global-company-mode)
-
-;;; eglot: LSP Support
-(unless (package-installed-p 'eglot)
-  (package-install 'eglot))
-(add-hook 'prog-mode-hook #'eglot-ensure)
-
-;;; flymake: Inline static analysis
-(add-hook 'prog-mode-hook #'flymake-mode)
-
-;;; magit: Git Client
-(unless (package-installed-p 'magit)
-  (package-install 'magit))
-(global-set-key (kbd "C-c g") #'magit-status)
-
-;;; diff-hl: Highlight VCS changes
-(unless (package-installed-p 'diff-hl)
-  (package-install 'diff-hl))
-(add-hook 'prog-mode-hook #'diff-hl-mode)
-
-;; Language Support
-;; (unless (package-installed-p 'go-mode)
-  ;; (package-install 'go-mode))
-
-;;; Python Support
-(unless (package-install-p 'python-mode)
-  (package-install 'python-mode))
-
-;;; JSON Support
-;; (unless (package-installed-p 'json-mode)
-  ;; (package-install 'json-mode))
-
-;;; YAML Support
-;; (unless (package-installed-p 'yaml-mode)
-  ;; (package-install 'yaml-mode))
-
-;;; LaTeX support
-;; (unless (package-installed-p 'auctex)
-  ;; (package-install 'auctex))
-;; (setq TeX-auto-save t)
-;; (setq TeX-parse-self t)
-;; (setq-default TeX-master nil)
-;; (add-hook 'LaTeX-mode-map #'LaTeX-math-mode) ; latex math support
-
-;;; Markdown support
-(unless (package-installed-p 'markdown-mode)
-  (package-install 'markdown-mode))
-
-;;; eat: In-Emacs Terminal Emulation
-(unless (package-installed-p 'eat)
-  (package-install 'eat))
-(setq eat-kill-buffer-on-exit t)
-(setq eat-enable-mouse t)
-
-;;; avy: Jump to arbitrary positions
-(unless (package-installed-p 'avy)
-  (package-install 'avy))
-(global-set-key (kbd "C-c z") #'avy-goto-word-1)
-
-;; Miscellaneous options
+;; Package Configuration
 (setq-default major-mode
               (lambda () ; guess major mode from file name
                 (unless buffer-file-name
@@ -91,12 +23,38 @@
 (savehist-mode t)
 (recentf-mode t)
 (defalias 'yes-or-no #'y-or-n-p)
-(setq backup-directory-alist `(("/" . "~/.backups")))
 (setq backup-by-copying t)
 (setq delete-old-versions t
       kept-new-versions 6
       kept-old-versions 2
       version-control t)
+(vertico-mode t) ; completing by narrowing
+(setq eat-kill-buffer-on-exit t)
+(setq eat-enable-mouse t)
+;; (setq TeX-auto-save t)
+;; (setq TeX-parse-self t)
+;; (setq-default TeX-master nil)
+;; (add-hook 'LaTeX-mode-map #'LaTeX-math-mode) ; latex math support
+
+;; Directories Used
+(setq org-agenda-files '("~/org"))
+(setq backup-directory-alist `(("/" . "~/.backups")))
+
+;; Hooks
+(add-hook 'after-init-hook 'global-company-mode)
+(add-hook 'prog-mode-hook #'eglot-ensure)
+(add-hook 'prog-mode-hook #'flymake-mode)
+(add-hook 'prog-mode-hook #'diff-hl-mode)
+
+
+;; Keybinds
+(global-set-key (kbd "C-c g") #'magit-status)
+(global-set-key (kbd "C-c z") #'avy-goto-word-1)
+(global-set-key (kbd "C-c l") #'org-store-link)
+(global-set-key (kbd "C-c a") #'org-agenda)
+(global-set-key (kbd "C-c c") #'org-capture)
+
+;; Miscellaneous options
 
 ;; Custom Start Screen
 (defun start-scr()
@@ -106,12 +64,6 @@
 (setq inhibit-startup-screen t
       initial-buffer-choice nil)
 
-;; Org-mode Shortcuts
-(global-set-key (kbd "C-c l") #'org-store-link)
-(global-set-key (kbd "C-c a") #'org-agenda)
-(global-set-key (kbd "C-c c") #'org-capture)
-
-(setq org-agenda-files '("~/org"))
-
 
 ;;; init.el ends here
+
